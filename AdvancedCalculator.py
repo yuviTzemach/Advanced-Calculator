d0cf1f55a5e58451d6fb6e73f4b8f8cb9cd49859
Yuval <yuvaltze2004@gmail.com>
2022-12-14 21:49:20 +0200

    the class is the main class in the project, that use all the functions and test ithem, and after it calculate to get the result

---
+++ AdvancedCalculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from StringModification import *
+from Functions import *
+
+
+def main():
+
+    string = "1--3!"
+    print("1")
+    string = erase_space(string)
+    print(string)
+    print("2")
+    validate_chars(string)
+    print(string)
+    print("3")
+    validate_parentheses(string)
+    print(string)
+    print("4")
+    string = erase_minus(string)
+    print(string)
+    validate_operands(string)
+    print(string)
+
+
+if __name__ == "__main__":
+    main()
