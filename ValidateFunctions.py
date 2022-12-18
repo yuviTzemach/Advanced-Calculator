0366a68ad2e76cecc343d51e58b5eff6b43f4f92
Yuval <yuvaltze2004@gmail.com>
2022-12-18 22:47:15 +0200

    updating the function of right and left operands

--- ValidateFunctions.py
+++ ValidateFunctions.py
@@ -1,20 +1,42 @@
+from Definitions import *
+
+
 class ValidateOperations:
+
+    def validate_left_operand_for_left_operator(op1):
+        """
+        the function is checking the validation of the left char next to left operator (#, !) - if op1 is ')' or a
+        digit, it raises an ArithmeticError
+        @param op1: the left operand
+        """
+        if (op1 == ')' or op1.isdigit()):
+            raise ArithmeticError("Left operand for left operator is not valid")
+
+    def validate_right_operand_for_right_operator(op2):
+        """
+        the function is checking the validation of the right char next to right operator (~) - if op2 is '(', '-' or a
+        digit, it raises an ArithmeticError
+        @param op2: the right operand
+        """
+        if (op2 == '(' or op2.isdigit()):
+            raise ArithmeticError("Right operand for right operator is not valid")
 
     def validate_left_operand(op1):
         """
         the function is checking the validation of the char - if op1 is not ')' or a digit, it raises an ArithmeticError
         @param op1: the left operand
         """
-        if not (op1 == ')' or op1.isdigit()):
+        # '!' and '#' are also allowed as the left operand of operators
+        if not (op1 == ')' or op1.isdigit() or op1 in right_operations):
             raise ArithmeticError("Left operand is not valid")
 
     def validate_right_operand(op2):
         """
-        the function is checking the validation of the char - if op1 is not '(', '-' or a digit, it raises an
+        the function is checking the validation of the char - if op2 is not '(', '-' or a digit, it raises an
         ArithmeticError
         @param op2: the right operand
         """
-        if not (op2 == '(' or op2 == '-' or op2.isdigit()):
+        if not (op2 == '(' or op2 == '-' or op2.isdigit() or op2 in left_operation):
             raise ArithmeticError("Right operand is not valid")
 
     def validate_trivial_operation(op1, op2):
