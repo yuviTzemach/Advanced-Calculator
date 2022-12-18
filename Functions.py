74b6e2790aab6921b1b0895518d59724980439a6
Yuval <yuvaltze2004@gmail.com>
2022-12-18 20:47:13 +0200

    modify functions to support float and infinty numbers

--- Functions.py
+++ Functions.py
@@ -3,6 +3,12 @@ from ValidateFunctions import *
 
 class MathOperations:
 
+    def validate_inf(number: str):
+        # infinity float number will contain 'e' character in it
+        if str(float(number)).find('e') != -1:
+            raise ValueError("Error : got infinity number !")
+        return True
+
     def add(op1, op2):
         """
         the function is checking if the chars next to the operator is valid, and if yes - returns the addition of it
@@ -10,8 +16,10 @@ class MathOperations:
         @param op2: the right operand
         returns the addition between the two operands
         """
-        ValidateOperations.validate_trivial_operation((op1, op2))
-        return op1 + op2
+        MathOperations.validate_inf(op1) and MathOperations.validate_inf(op2)
+        res = str(float(op1) + float(op2))
+        MathOperations.validate_inf(res)
+        return res
 
     def sub(op1, op2):
         """
@@ -20,8 +28,10 @@ class MathOperations:
         @param op2: the right operand
         returns the subtraction between the two operands
         """
-        ValidateOperations.validate_trivial_operation((op1, op2))
-        return op1 - op2
+        MathOperations.validate_inf(op1) and MathOperations.validate_inf(op2)
+        res = str(float(op1) - float(op2))
+        MathOperations.validate_inf(res)
+        return res
 
     def mul(op1, op2):
         """
@@ -31,8 +41,10 @@ class MathOperations:
         @param op2: the right operand
         returns the multiplication between the two operands
         """
-        ValidateOperations.validate_trivial_operation((op1, op2))
-        return op1 * op2
+        MathOperations.validate_inf(op1) and MathOperations.validate_inf(op2)
+        res = str(float(op1) * float(op2))
+        MathOperations.validate_inf(op2)
+        return res
 
     def div(op1, op2):
         """
@@ -41,8 +53,12 @@ class MathOperations:
         @param op2: the right operand
         returns the division between the two operands
         """
-        ValidateOperations.validate_div((op1, op2))
-        return op1 / op2
+        if float(op2) == 0.0:
+            raise ArithmeticError("Dividing by zero")
+        MathOperations.validate_inf(op1) and MathOperations.validate_inf(op2)
+        res = str(float(op1) / float(op2))
+        MathOperations.validate_inf(res)
+        return res
 
     def power(op1, op2):
         """
@@ -51,13 +67,13 @@ class MathOperations:
         @param op2: the right operand
         returns the power between the two operands
         """
-        ValidateOperations.validate_trivial_operation(op1, op2)
-        # checking if the result of the power is a complex number - if yes, it raises an error, if no, it returns is
-        result = pow(op1, op2)
-        if isinstance(result, complex):
+        MathOperations.validate_inf(op1) and MathOperations.validate_inf(op2)
+        res = pow(float(op1), float(op2))
+        if isinstance(res, complex):
+            # checking if the result of the power is a complex number - if yes, it raises an error, if no, it returns is
             raise ArithmeticError("the result is a complex number")
-        else:
-            return result
+        MathOperations.validate_inf(res)
+        return res
 
     def modulo(op1, op2):
         """
@@ -66,8 +82,10 @@ class MathOperations:
         @param op2: the right operand
         returns the modulo between the two operands
         """
-        ValidateOperations.validate_trivial_operation((op1, op2))
-        return op1 % op2
+        MathOperations.validate_inf(op1) and MathOperations.validate_inf(op2)
+        res = str(float(op1) % float(op2))
+        MathOperations.validate_inf(res)
+        return res
 
     def max(op1, op2):
         """
@@ -77,11 +95,8 @@ class MathOperations:
         @param op2: the right operand
         returns the maximum operand between the two operands
         """
-        ValidateOperations.validate_trivial_operation((op1, op2))
-        if (op1 > op2):
-            return op1
-        else:
-            return op2
+        MathOperations.validate_inf(op1) and MathOperations.validate_inf(op2)
+        return str(float(op1)) if float(op1) > float(op2) else str(float(op2))
 
     def min(op1, op2):
         """
@@ -91,11 +106,8 @@ class MathOperations:
         @param op2: the right operand
         returns the minimum operand between the two operands
         """
-        ValidateOperations.validate_trivial_operation((op1, op2))
-        if (op1 < op2):
-            return op1
-        else:
-            return op2
+        MathOperations.validate_inf(op1) and MathOperations.validate_inf(op2)
+        return str(float(op1)) if float(op1) < float(op2) else str(float(op2))
 
     def avg(op1, op2):
         """
@@ -104,18 +116,22 @@ class MathOperations:
         @param op2: the right operand
         returns the average between the two operands
         """
-        ValidateOperations.validate_trivial_operation((op1, op2))
-        return ((op1 + op2) / 2)
+        MathOperations.validate_inf(op1) and MathOperations.validate_inf(op2)
+        res = str(float((float(op1) + float(op2)) / 2.0))
+        MathOperations.validate_inf(res)
+        return res
 
-    def tilda(op1):
+    def tilda(op2):
         """
         the function is checking if the right char next to the operator is valid, and if yes - returns the negation of
         it
-        @param op1: the right operand
+        @param op2: the right operand
         returns the negation of the operand
         """
-        ValidateOperations.validate_tilda(op1)
-        return op1 * -1
+        MathOperations.validate_inf(op2)
+        res = str(float(op2) * -1)
+        MathOperations.validate_inf(res)
+        return res
 
     def factorial(op1):
         """
@@ -124,11 +140,26 @@ class MathOperations:
         @param op1: the left operand
         returns the factorial of the operand
         """
-        ValidateOperations.validate_factorial(op1)
-        fact = 1
-        for x in range(1, op1 + 1):
-            fact = fact * x
-        return fact
+        MathOperations.validate_inf(op1)
+        # if op1 represents like a float
+        if op1.find('.') != -1:
+            # split the float number by its floating point
+            op1, after_floating_point = op1.split('.')
+            # check if the number after the floating point is 0.
+            # if not, op1 isn't an integer and the operation isn't valid.
+            if after_floating_point != "0":
+                raise ArithmeticError("doesn't work on float numbers")
+        # it is ok because op1 is definitely an integer here
+        op1 = int(op1)
+        factorial = 1
+        if op1 < 0:
+            raise ArithmeticError("doesn't work on negative numbers")
+        elif op1 == 0:
+            return str(float(factorial))
+        for i in range(1, op1 + 1):
+            factorial *= i
+        MathOperations.validate_inf(factorial)
+        return str(float(factorial))
 
     def sum_numbers(op1):
         """
@@ -137,10 +168,13 @@ class MathOperations:
         @param op1: the left operand
         returns the sum of the numbers the operand contains
         """
-        ValidateOperations.validate_sum_numbers(op1)
-        op1 = int(str(op1).replace('.', ''))
-        sum1 = 0
+        # remove the '.' from the operand
+        MathOperations.validate_inf(op1)
+        op1 = int((op1.replace('.', '')))
+        sum = 0
         while op1 != 0:
-            sum1 += (op1 % 10)
-            op1 /= 10
-        return sum1
+            sum += (op1 % 10)
+            op1 //= 10
+        res = str(float(sum))
+        MathOperations.validate_inf(res)
+        return res
