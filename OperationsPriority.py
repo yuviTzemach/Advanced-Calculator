55a2548b0f80720757259227d5515d15cdca4bca
Yuval <yuvaltze2004@gmail.com>
2022-12-19 13:57:02 +0200

    adding docs files

--- OperationsPriority.py
+++ OperationsPriority.py
@@ -1,8 +1,17 @@
 from ExpressionParser import *
 from Definitions import *
 
+"""
+The class is calculating the priority of each operator.
+"""
+
 
 def factorial_sum_tilda_priority(string):
+    """
+    The function is calculating the priority '!', '#' and '~' - in this case, 6 priority
+    @param string: the string
+    returns by calling the one_operand_operator_priority function with the string and the operator index
+    """
     factorial_index = string.find('!')
     sum_index = string.find('#')
     tilda_index = string.find('~')
@@ -11,6 +20,13 @@ def factorial_sum_tilda_priority(string):
 
 
 def power_priority(string):
+    """
+    The function is calculating the priority '^' - in this case, 3 priority
+    @param string: the string
+    if power_index != -1, returns true, the operator, and calling the split_expression_to_two_operands function with
+    the string and the index of the operator
+    else, return false, '', ()
+    """
     power_index = string.find('^')
     if power_index == -1:
         return False, '', ()
@@ -19,6 +35,13 @@ def power_priority(string):
 
 
 def modulo_priority(string):
+    """
+    The function is calculating the priority '%' - in this case, 4 priority
+    @param string: the string
+    if power_index != -1, returns true, the operator, and calling the split_expression_to_two_operands function with
+    the string and the index of the operator
+    else, return false, '', ()
+    """
     modulo_index = string.find('%')
     if modulo_index == -1:
         return False, '', ()
@@ -27,6 +50,11 @@ def modulo_priority(string):
 
 
 def min_max_avg_priority(string):
+    """
+    The function is calculating the priority '&', '$' and '@' - in this case, 5 priority
+    @param string: the string
+    returns by calling the two_operands_operator_priority function with the string and the operators index
+    """
     min_index = string.find('&')
     max_index = string.find('$')
     avg_index = string.find('@')
@@ -35,6 +63,11 @@ def min_max_avg_priority(string):
 
 
 def mul_div_priority(string):
+    """
+    The function is calculating the priority '*', and '/' - in this case, 2 priority
+    @param string: the string
+    returns by calling the two_operands_operator_priority function with the string and the operators index
+    """
     mul_index = string.find('*')
     div_index = string.find('/')
     operators_index = {mul_index: '*', div_index: '/'}
@@ -42,6 +75,11 @@ def mul_div_priority(string):
 
 
 def plus_minus_priority(string):
+    """
+    The function is calculating the priority '+', and '-' - in this case, 1 priority
+    @param string: the string
+    returns by calling the two_operands_operator_priority function with the string and the operators index
+    """
     plus_index = string.find('+')
     minus_index = ExpressionParser.find_first_minus_operator(string)
     operators_index = {plus_index: '+', minus_index: '-'}
@@ -78,7 +116,7 @@ def one_operand_operator_priority(string, operators_index):
     returns the operator and the operands that were extracted from the sides of the operator.
     @param string: the expression to act on
     @param operators_index: a dictionary of index - operator.
-    (see 'find_first_operator_to_execute' for more explaination)
+    (see 'find_first_operator_to_execute' for more explanation)
     returns If found an operator:
             True, operator, (left_operand, left_operand_index, right_operand, right_operand_index)
             If not:
